195f865a688e7150764b8614685b0934b8c71681
Hecta18 <62799494+Hecta18@users.noreply.github.com>
2025-02-19 17:10:08 -0600

    JUnit testing Stack & InToPost

---
+++ Tests.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+public class Tests {
+    @Test
+    public void test1() {
+        System.out.println(InToPost.infixToPostfix("A + B * C"));
+    }
+
+    @Test
+    public void test2() {
+        Stack<String> stack = new Stack<String>();
+        stack.push("A");
+        System.out.println(stack.size() + "/n" +
+        stack.peek() + "/n" +
+        stack.pop() + "/n" +
+        stack.isEmpty());
+    }
+}
