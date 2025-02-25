eb7cde8c6e0ea9c7a6bdea89aa004ee98cac8b74
Hecta18 <62799494+Hecta18@users.noreply.github.com>
2025-02-24 23:11:23 -0600

    implementar lectura de .txt

--- Main.java
+++ Main.java
@@ -1,13 +1,19 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
+        System.out.println("Enter the file path: /n");
+        try (BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in))) {
+            String filePath = br1.readLine();
+            try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String input = br.readLine();
             System.out.println(InToPost.infixToPostfix(input));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
